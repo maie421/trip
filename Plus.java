af598863794183caeff99a63cbdcbb057a5ce9a2
윤영미 <35258834+maie421@users.noreply.github.com>
2019-03-10 16:12:28 +0900

    나라 추가

--- Plus.java
+++ Plus.java
@@ -111,7 +111,7 @@ public class Plus extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pls);
+        setContentView(R.layout.activity_plus);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
