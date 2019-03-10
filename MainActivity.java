429a92ed8a44cae000fadf81de041484a3a7aab4
윤영미 <35258834+maie421@users.noreply.github.com>
2019-03-10 15:37:07 +0900

    로그인 화면

---
+++ MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.admin.trip;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button join=(Button)findViewById(R.id.join);
+        Button menu=(Button)findViewById(R.id.button);
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),join.class);
+                startActivity(intent);
+            }
+        });
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),menu1.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
